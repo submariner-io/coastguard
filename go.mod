e374d63fbfccbc0cf00d2ee4c318faa1e0210c18
Miguel Angel Ajo <majopela@redhat.com>
2019-07-31 14:01:03 +0200

    Switch the filediscovery to kubefed federator from admiral
    This commit switches the debug filediscovery to kubefed cluster
    discovery.

--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/pkg/errors v0.8.1
 	github.com/submariner-io/admiral v0.0.0-20190717183806-7f0ad3dca605
 	github.com/submariner-io/submariner v0.0.0-20190708095718-350482d85dd4
-	golang.org/x/net v0.0.0-20190603091049-60506f45cf65
 	k8s.io/api v0.0.0-20190222213804-5cb15d344471
 	k8s.io/apimachinery v0.0.0-20190629003722-e20a3a656cff
 	k8s.io/client-go v0.0.0-20190521190702-177766529176
--- pkg/coastguard/main.go
+++ pkg/coastguard/main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"flag"
+	"os"
 	"sync"
 
+	"github.com/submariner-io/admiral/pkg/federate"
 	"github.com/submariner-io/submariner/pkg/signals"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog"
 
-	"github.com/submariner-io/coastguard/pkg/clusters/discovery"
+	"github.com/submariner-io/admiral/pkg/federate/kubefed"
+
 	"github.com/submariner-io/coastguard/pkg/controller"
 )
+
+var kubeConfig string
+var masterURL string
+
+func init() {
+	flag.StringVar(&kubeConfig, "kubeconfig", os.Getenv("KUBECONFIG"),
+		"Path to kubeconfig containing embedded authinfo.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+
+}
 
 func main() {
 	klog.InitFlags(nil)
@@ -25,14 +39,30 @@ func main() {
 	wg.Add(1)
 
 	coastGuardController := controller.New()
-
 	go func() {
 		defer wg.Done()
 		coastGuardController.Run(stopCh)
 	}()
 
-	discovery.Start(coastGuardController)
+	federator := buildKubeFedFederator(stopCh)
+	err := federator.WatchClusters(coastGuardController)
+
+	if err != nil {
+		klog.Fatalf("Error watching federation clusters: %s", err.Error())
+	}
 
 	wg.Wait()
 	klog.Fatal("All controllers stopped or exited. Stopping main loop")
+}
+
+func buildKubeFedFederator(stopCh <-chan struct{}) federate.Federator {
+	kubeConfig, err := clientcmd.BuildConfigFromFlags(masterURL, kubeConfig)
+	if err != nil {
+		klog.Fatalf("Error attempting to load kubeconfig: %s", err.Error())
+	}
+	federator, err := kubefed.New(kubeConfig, stopCh)
+	if err != nil {
+		klog.Fatalf("Error creating kubefed federator: %s", err.Error())
+	}
+	return federator
 }
